f633f011b5fad87f40005afc55be8371ab711943
d.pasquali <d.pasquali@reply.it>
2018-02-23 12:07:23 +0100

    fix

--- worker/worker.tf
+++ worker/worker.tf
@@ -11,7 +11,7 @@ resource "google_compute_instance" "worker" {
  name = "terraform-test"
  machine_type = "n1-standard-1"
 
- tags = ["jenkins", "cloudera", "http", "https"]
+ tags = ["worker", "support", "http", "https"]
  
  boot_disk {
    initialize_params {
@@ -28,7 +28,11 @@ resource "google_compute_instance" "worker" {
 
  provisioner "local-exec" { 
   #command = "sleep 90; ansible-playbook -i '${google_compute_instance.worker.name},' --private-key=~/.ssh/ansible_rsa /opt/ansible/worker.yml -e 'ansible_ssh_user=dario_pasquali93' -e 'host_key_checking=False'"
-  command = "sleep 90; ansible -i '${google_compute_instance.worker.name},' all --private-key=~/.ssh/ansible_rsa -m ping -e 'ansible_ssh_user=dario_pasquali93' -e 'host_key_checking=False'"
+  command = "sleep 90; ansible -i '${google_compute_instance.worker.name},'
+   all --private-key=ssh/private/key/path
+   -m ping
+   -e 'ansible_ssh_user=dario_pasquali93'
+   -e 'host_key_checking=False'"
  }
 }
 
