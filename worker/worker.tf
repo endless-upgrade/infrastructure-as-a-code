a7325d559b24cff994ce53eca3645b206342fc5e
Dario Pasquali <32868701+dpasqualiReply@users.noreply.github.com>
2018-02-28 21:07:00 +0100

    Update worker.tf

--- worker/worker.tf
+++ worker/worker.tf
@@ -29,7 +29,7 @@ resource "google_compute_instance" "worker" {
 # provisioner "local-exec" { 
 #  command = "sleep 90; ansible -i '${google_compute_instance.worker.name},' all --private-key=ssh/private/key/path -m ping -e 'ansible_ssh_user=dario_pasquali93' -e 'host_key_checking=False'" 
 #  }
-#}
+}
 
 output "worker" {
  value = "${google_compute_instance.worker.self_link}"
